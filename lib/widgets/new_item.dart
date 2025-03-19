6f28e3b2182b96cfd5d5a3857cbc840bb180dc13
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2025-03-19 23:12:51 +0530

    Expanded added

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -34,34 +34,38 @@ class _NewItemState extends State<NewItem>{
                         ),
                         Row(
                           children: [
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                label: Text('Quantity'),
+                            Expanded(
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  label: Text('Quantity'),
+                                ),
+                                initialValue: '1',
                               ),
-                              initialValue: '1',
                             ),
                             const SizedBox(width: 8),
-                            DropdownButtonFormField(
-                              items: [
-                                for(final category in categories.entries)
-                                DropdownMenuItem(
-                                  value: category.value,
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                      width: 16,
-                                      height: 16,
-                                      color: category.value.color,
+                            Expanded(
+                              child: DropdownButtonFormField(
+                                items: [
+                                  for(final category in categories.entries)
+                                  DropdownMenuItem(
+                                    value: category.value,
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                        width: 16,
+                                        height: 16,
+                                        color: category.value.color,
+                                      ),
+                                      const SizedBox(width: 6),
+                                      Text(category.value.title)
+                                      ],
                                     ),
-                                    const SizedBox(width: 6),
-                                    Text(category.value.title)
-                                    ],
                                   ),
-                                ),
-                              ], 
-                              // onChanged: (value){
-                                
-                              // }
+                                ], 
+                                onChanged: (value){
+                                  
+                                }
+                              ),
                             ),
                           ],
                         ),
